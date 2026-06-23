ee7cf5f5473dbc7636c84ea91d3544eb735dc539
vanmeetech <vanfindme@gmail.com>
2026-06-24 00:33:09 +0800

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,10 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-export default defineConfig({ output: "static", integrations: [react()] });
+
+export default defineConfig({
+  output: "static",
+  site: "https://vanmee.cn",
+  base: "/",
+  trailingSlash: "always",
+  integrations: [react()]
+});
